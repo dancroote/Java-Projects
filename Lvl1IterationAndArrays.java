f75a1d36128b21d0941f36d4fedb120e9242a268
Dan Croote <dancroote@outlook.com>
2018-02-06 10:33:07 +0200

    Second batch of lvl1

---
+++ Lvl1IterationAndArrays.java
@@ -0,0 +1,12 @@
+
+public class Lvl1IterationAndArrays {
+
+	public static void main(String[] args) {
+		int[] arrayOfInts = {0,1,2,3,4,5,6,7,8,9};
+		
+		for (int counter = 0; counter < 10; counter++) {
+			System.out.println(arrayOfInts[counter]);
+		}
+	}
+
+}
